a2a2aa5a45476147517416565dba8a6cec0ad0d3
Gabriel Siles <gabosiles23@gmail.com>
2024-10-15 13:59:11 -0600

    "Se genera la opcion apra que unicamente se gaurde una línea"

--- api/inicio_gps.py
+++ api/inicio_gps.py
@@ -9,15 +9,17 @@ output_file = 'gps_data.json'  # Archivo JSON donde se guardarán los datos
 ser = serial.Serial(port, baudrate, timeout=10)  # Se abre puerto serial
 
 try:
-    with open(output_file, 'a') as file:  # Abre el archivo en modo 'append'
-        print(f"Guardando datos en {output_file}. Presione CTRL+C para salir.")
-        while True:
-            line = ser.readline().decode('ascii', errors='replace').strip()  # Lee datos del puerto serial
-            if line.startswith('$GPRMC'):  # Verifica si la línea comienza con $GPRMC
-                print(line)  # Muestra los datos en la consola
-                json_data = {"gps_data": line}  # Estructura los datos en un diccionario para JSON
+    print(f"Guardando datos en {output_file}. Presione CTRL+C para salir.")
+    while True:
+        line = ser.readline().decode('ascii', errors='replace').strip()  # Lee datos del puerto serial
+        if line.startswith('$GPRMC'):  # Verifica si la línea comienza con $GPRMC
+            print(line)  # Muestra los datos en la consola
+            json_data = {"gps_data": line}  # Estructura los datos en un diccionario para JSON
+            
+            # Abre el archivo en modo 'w' para sobrescribir con la nueva línea
+            with open(output_file, 'w') as file:
                 json.dump(json_data, file)  # Escribe los datos en formato JSON en el archivo
-                file.write('\n')  # Añade una nueva línea después de cada entrada JSON
+
 except KeyboardInterrupt:
     print("\nPrograma interrumpido. Cerrando...")
 finally:
