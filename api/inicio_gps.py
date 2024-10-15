fb26beed264bac4f5d1d765d6c7760a6ffa19cf2
Gabriel Siles <gabosiles23@gmail.com>
2024-10-15 14:14:44 -0600

    "Version con el registro txt"

--- api/inicio_gps.py
+++ api/inicio_gps.py
@@ -2,23 +2,28 @@ import serial
 import json
 
 # Configuración puerto serial
-port = '/dev/serial0'  # Se define puerto
-baudrate = 9600        # Frecuencia/velocidad de trabajo
-output_file = 'gps_data.json'  # Archivo JSON donde se guardarán los datos
+port = '/dev/serial0'               # Se define puerto
+baudrate = 9600                     # Frecuencia/velocidad de trabajo
+json_output_file = 'gps_data.json'  # Archivo JSON donde se sobrescriben los datos
+txt_output_file = 'gps_data.txt'    # Archivo de texto donde se guardarán los datos con append
 
 ser = serial.Serial(port, baudrate, timeout=10)  # Se abre puerto serial
 
 try:
-    print(f"Guardando datos en {output_file}. Presione CTRL+C para salir.")
+    print(f"Guardando datos en {json_output_file} y {txt_output_file}. Presione CTRL+C para salir.")
     while True:
-        line = ser.readline().decode('ascii', errors='replace').strip()  # Lee datos del puerto serial
-        if line.startswith('$GPRMC'):  # Verifica si la línea comienza con $GPRMC
-            print(line)  # Muestra los datos en la consola
-            json_data = {"gps_data": line}  # Estructura los datos en un diccionario para JSON
+        line = ser.readline().decode('ascii', errors='replace').strip() # Lee datos del puerto serial
+        if line.startswith('$GPRMC'):                                   # Verifica si la línea comienza con $GPRMC
+            print(line)                                                 # Muestra los datos en la consola
+            json_data = {"gps_data": line}                              # Estructura los datos en un diccionario para JSON
             
-            # Abre el archivo en modo 'w' para sobrescribir con la nueva línea
-            with open(output_file, 'w') as file:
-                json.dump(json_data, file)  # Escribe los datos en formato JSON en el archivo
+            # Guardar en archivo JSON (sobrescribiendo)
+            with open(json_output_file, 'w') as json_file:
+                json.dump(json_data, json_file)                         # Escribe los datos en formato JSON en el archivo
+
+            # Guardar en archivo de texto (append)
+            with open(txt_output_file, 'a') as txt_file:
+                txt_file.write(line + '\n')                             # Guarda la línea en el archivo de texto
 
 except KeyboardInterrupt:
     print("\nPrograma interrumpido. Cerrando...")
