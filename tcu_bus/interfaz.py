ec6ade9f2176e7a2f6c2bf66f68ceea042789839
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-11-19 15:51:24 -0600

    Adición de caracteres faltantes a teclado y limpieza de branch.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- tcu_bus/interfaz.py
+++ tcu_bus/interfaz.py
@@ -94,7 +94,7 @@ class InterfazMain(tk.Tk, funcRegistro):
             '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
             'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
             'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ñ',
-            'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BORRAR'
+            'Z', 'X', 'C', 'V', 'B', 'N', 'M', '@', 'BORRAR'
         ]
 
         keyboard_frame = tk.Frame(self.keyboard_frame)
